bbe9314ee0505d4447866c1dbd42ce6f176b7f0b
Davin <davinkusno@gmail.com>
2025-01-20 23:12:23 +0700

    fix: bootstrap import

--- resources/views/layouts/navbar/navbar.blade.php
+++ resources/views/layouts/navbar/navbar.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
-    <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> -->
     <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
 
 
